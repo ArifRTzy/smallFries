0d14e166c2a524f77933b6ac7d42b86505cc7916
ArifR_ <arifr0751@gmail.com>
2024-10-29 00:02:38 +0700

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Home from "./components/Home";
+import Comp from "./components/Comp";
 
 const App = () => {
   return (
-    <div className="">
-      <Navbar/>
-      <Sidebar/>
-    </div>
-  )
-}
+    <Router>
+      <div className="h-[200vh]">
+        <Navbar />
 
-export default App
+        <div className="flex-grow">
+          <Routes>
+            {/* Sidebar layout */}
+            <Route path="/" element={<Sidebar />}>
+              {/* Sidebar child routes */}
+              <Route index element={<Home />} />
+              <Route path="comp" element={<Comp />} />
+            </Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import { Link, Outlet } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className='pt-[60px] w-full'>
-        <div className="w-[90%] mx-auto">
-            <div className="w-[20%] h-[100vh]">
-                <div className="">
-                    <input type="text" className='border-2'/>
-                </div>
-                <div className="">
-                    Com1
-                </div>
-            </div>
+    <div className="pt-[60px] w-full">
+      <div className="w-[90%] mx-auto flex">
+        <div className="w-[20%] max-h-[100vh] overflow-auto fixed">
+          <div className="h-[150vh]">
+            <input type="text" className="border-2" />
+          </div>
+          <div className="">
+            <li>
+              <Link to="/">home</Link>
+            </li>
+            <li>
+              <Link to="/comp">comp</Link>
+            </li>
+          </div>
         </div>
+        <div className="w-[80%] ml-[25%]">
+          <Outlet />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;

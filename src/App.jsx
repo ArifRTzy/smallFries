e7861a7475c6b06b6bfe41bb378d5bab2e9bf726
ArifR_ <arifr0751@gmail.com>
2024-10-29 00:06:30 +0700

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,8 @@ import Comp from "./components/Comp";
 const App = () => {
   return (
     <Router>
-<<<<<<< HEAD
       <div className="h-[200vh]">
         <Navbar />
-=======
-
-<div className="">
-      <Navbar/>
-     
-      <div className="flex-grow">
-          <Routes>
-            {/* Sidebar layout */}
-            <Route path="/" element={<Sidebar />}>
-              {/* Sidebar child routes */}
-              <Route index element={<Home />} />
-              <Route path="comp" element={<Comp />} />
-            </Route>
-          </Routes>
-        </div>
-
-    </div>
-
-
-  </Router>
-
-  )
-}
->>>>>>> origin/main
 
         <div className="flex-grow">
           <Routes>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link, Outlet } from "react-router-dom";
 
 const Sidebar = () => {
@@ -17,30 +16,6 @@ const Sidebar = () => {
               <Link to="/comp">comp</Link>
             </li>
           </div>
-=======
-import { Link, Outlet } from 'react-router-dom';
-
-const Sidebar = () => {
-  return (       
-    <div className='pt-[60px] w-full'>
-        <div className="w-[90%] mx-auto flex">
-            <div className="w-[20%] h-[150vh] overflow-auto">
-                <div className="">
-                    <input type="text" className='border-2'/>
-                </div>
-                <div className="">
-                    <li >
-                        <Link to="/">home</Link>
-                    </li>
-                    <li>
-                        <Link to="/comp">comp</Link>
-                    </li>
-                </div>
-            </div>
-            <div className="w-[80%]">
-        <Outlet />
-      </div>
->>>>>>> origin/main
         </div>
         <div className="w-[80%] ml-[25%]">
           <Outlet />

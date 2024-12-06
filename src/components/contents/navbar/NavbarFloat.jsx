338fbcfa44ee780719ba5d83e32c106ed4cfd4c5
ArifR_ <arifr0751@gmail.com>
2024-12-06 23:28:13 +0700

    initial

--- src/components/contents/navbar/NavbarFloat.jsx
+++ src/components/contents/navbar/NavbarFloat.jsx
@@ -1,4 +1,5 @@
 import { menuComponents } from "../../../constants";
+import { laptop } from "../../../utils";
 
 const NavbarFloat = () => {
   return (
@@ -6,12 +7,13 @@ const NavbarFloat = () => {
       <p className="font-semibold text-slate-600">{menuComponents[1].title}</p>
       <h1 className="comps-h1">Navbar Float</h1>
       <p className="comps-desc">Responsive floating navbar</p>
+      <p className="text-xl font-semibold">Preview</p>
       <div className="">
-        <div className="">
-          <img src="" alt="" />
-          <p className="text-xl font-semibold">Preview</p>
+        <div className="flex items-center">
+          <img src={laptop} alt="laptop" className="w-7"/>
+          <p className="text-lg font-medium">PC/Laptop</p>
         </div>
-        <div className="w-full border-2 rounded-lg min-h-[350px] bg-[repeating-linear-gradient(0deg,transparent,transparent_10px,rgba(0,0,0,0.3)_11px),repeating-linear-gradient(90deg,transparent,transparent_10px,rgba(0,0,0,0.3)_11px)]">
+        <div className="w-full border-2 rounded-lg min-h-[350px] bg-[repeating-linear-gradient(0deg,transparent,transparent_5px,rgba(0,0,0,0.101)_6px),repeating-linear-gradient(90deg,transparent,transparent_5px,rgba(0,0,0,0.101)_6px)]">
 
         </div>
       </div>
--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,35 +1,39 @@
-import menuDots from "/images/dots-three-vertical.svg"
-import menuLists from "/images/list.svg"
-import searchs from "/images/magnifying-glass.svg"
-import githubs from "/images/github.svg"
-import lights from "/images/sun.svg"
-import darks from "/images/moon.svg"
-import systems from "/images/pc.svg"
-import lightsBlues from "/images/sun-blue.svg"
-import darksBlues from "/images/moon-blue.svg"
-import systemsBlues from "/images/pc-blue.svg"
-import closes from "/images/close.svg"
-import dropdowns from "/images/caret-down.svg"
-import dropdownsWhite from "/images/caret-down-white.svg"
-import codes from "/images/code.svg"
-import codesWhite from "/images/code-white.svg"
-import cats from "/images/cat.jpg"
-import catGifs from "/images/happy-cat.gif"
+import menuDots from "/images/dots-three-vertical.svg";
+import menuLists from "/images/list.svg";
+import searchs from "/images/magnifying-glass.svg";
+import githubs from "/images/github.svg";
+import lights from "/images/sun.svg";
+import darks from "/images/moon.svg";
+import systems from "/images/pc.svg";
+import lightsBlues from "/images/sun-blue.svg";
+import darksBlues from "/images/moon-blue.svg";
+import systemsBlues from "/images/pc-blue.svg";
+import closes from "/images/close.svg";
+import dropdowns from "/images/caret-down.svg";
+import dropdownsWhite from "/images/caret-down-white.svg";
+import codes from "/images/code.svg";
+import codesWhite from "/images/code-white.svg";
+import cats from "/images/cat.jpg";
+import catGifs from "/images/happy-cat.gif";
+import laptops from "/images/laptop.svg";
+import phones from "/images/phone.svg";
 
-export const menuDot = menuDots
-export const menuList = menuLists
-export const search = searchs
-export const github = githubs
-export const light = lights
-export const dark = darks
-export const system = systems
-export const lightsBlue = lightsBlues
-export const darksBlue = darksBlues
-export const systemsBlue = systemsBlues
-export const close = closes
-export const dropdown = dropdowns
-export const dropdownWhite = dropdownsWhite
-export const code = codes
-export const codeWhite = codesWhite
-export const cat = cats
-export const catGif = catGifs
+export const menuDot = menuDots;
+export const menuList = menuLists;
+export const search = searchs;
+export const github = githubs;
+export const light = lights;
+export const dark = darks;
+export const system = systems;
+export const lightsBlue = lightsBlues;
+export const darksBlue = darksBlues;
+export const systemsBlue = systemsBlues;
+export const close = closes;
+export const dropdown = dropdowns;
+export const dropdownWhite = dropdownsWhite;
+export const code = codes;
+export const codeWhite = codesWhite;
+export const cat = cats;
+export const catGif = catGifs;
+export const laptop = laptops;
+export const phone = phones;
